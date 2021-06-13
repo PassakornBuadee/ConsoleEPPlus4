cc56f29f31145a9e88257a726806cd95adbd8509
Passakorn Buadee <Passakorn_b@mfec.co.th>
2021-06-13 21:59:16 +0700

    Add project files.

---
+++ ConsoleEPPlus4/Program.cs
@@ -0,0 +1,35 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+
+namespace ConsoleEPPlus4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvFileName = @"test.csv";
+            string excelFileName = @"test.xls";
+
+            string worksheetsName = "TEST";
+
+            bool firstRowIsHeader = false;
+
+            var format = new ExcelTextFormat();
+            format.Delimiter = ',';
+            format.EOL = "\r";              
+            // DEFAULT IS "\r\n";             
+            // format.TextQualifier = '"';
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFileName)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(worksheetsName);
+                worksheet.Cells["A1"].LoadFromText(new FileInfo(csvFileName), format, OfficeOpenXml.Table.TableStyles.Medium27, firstRowIsHeader);
+                package.Save();
+            }
+
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+        }
+    }
+}
